44accdd59fbb5a01035f7f549cc54c2bc63291cd
Maximilian Burr <maximilian.burr@outlook.de>
2023-05-31 22:29:00 +0200

    Refactord Loader Class because too long methods and prepared for jar loader

--- bridge/core/src/main/java/pluginmanager/Loader.java
+++ bridge/core/src/main/java/pluginmanager/Loader.java
@@ -9,20 +9,25 @@ import start.ICore;
 
 import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.net.URLClassLoader;
 import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.List;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
 
 public class Loader implements IPluginLoader {
 
-    private final String pluginDirectory;
+    private final File pluginDirectory;
     private final String pluginNamespace;
     private final IPluginFactory pluginFactory;
     private final List<IPlugin> pluginList;
 
     private final IConfiguration configuration;
+    private final ClassLoader bundledClassLoader;
 
 
     /**
@@ -30,71 +35,118 @@ public class Loader implements IPluginLoader {
      * @param pluginDirectory This parameter is the path to the directory where the plugins are located
      */
 
-    public Loader(String namespace, String pluginDirectory) {
+    public Loader(String namespace, File pluginDirectory) {
         this.pluginDirectory = pluginDirectory;
         this.pluginNamespace = namespace;
         this.pluginList = new ArrayList<IPlugin>();
         this.pluginFactory = new PluginFactory();
         this.configuration = new Configuration();
+        this.bundledClassLoader = Thread.currentThread().getContextClassLoader();
+
     }
 
 
     @Override
     public void loadPlugins() {
-        String path = pluginNamespace.replace('.', '/');
-        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-
-
         try {
-            //Get all Ressource in Classpath
-            Enumeration<URL> resources = classLoader.getResources(path);
-            while (resources.hasMoreElements()) {
-                URL classPath = resources.nextElement();
-                File file = new File(classPath.toURI());
-                if (file.isDirectory()) {
-                    //All Plugins in checked directory
-                    File[] pluginRoot = file.listFiles();
-                    for (File pluginDirectory : pluginRoot) {
-                        if (pluginDirectory.isDirectory()) {
-                            loadFromBundle(pluginDirectory.listFiles(), pluginDirectory.getName());
-                        }
-                    }
-                }
-            }
+            loadFromInternalClasspath();
+            loadFromExternalClassPath(pluginDirectory);
         } catch (URISyntaxException e) {
             System.out.println(e.getMessage());
         } catch (ClassNotFoundException e) {
             System.err.println("Check your plugins, Plugin class could not be found");
         } catch (IOException e) {
             System.err.println("Check your plugin folder/filesystem");
+        } catch (InvocationTargetException e) {
+            throw new RuntimeException(e);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        } catch (InstantiationException e) {
+            throw new RuntimeException(e);
+        } catch (NoSuchMethodException e) {
+            throw new RuntimeException(e);
         }
-
-        return;
     }
 
-    private void loadFromBundle(File[] pluginFileList, String pluginName) throws ClassNotFoundException {
+    private void loadFromExternalClassPath(File pluginDirectory) throws IOException, ClassNotFoundException, InvocationTargetException, IllegalAccessException, InstantiationException, NoSuchMethodException {
+        File[] jarFiles = pluginDirectory.listFiles((dir, name) -> name.endsWith(".jar"));
+
+        if (jarFiles != null) {
+            for (File jarFile : jarFiles) {
+                loadFromJarFile(jarFile);
+            }
+        }
+
+
+    }
+
+    private void loadFromJarFile(File jarFile) throws IOException, ClassNotFoundException, InvocationTargetException, IllegalAccessException, InstantiationException, NoSuchMethodException {
+        URLClassLoader jarClassLoader = URLClassLoader.newInstance(new URL[]{jarFile.toURI().toURL()});
+        JarFile jar = new JarFile(jarFile);
+        Enumeration<JarEntry> entries = jar.entries();
+
+        while (entries.hasMoreElements()) {
+            JarEntry entry = entries.nextElement();
+            if (entry.isDirectory() || !entry.getName().endsWith(".class")) {
+                continue;
+            }
+            String className = entry.getName().substring(0, entry.getName().length() - 6).replace('/', '.');
+            createPlugin(className);
+        }
+        jar.close();
+
+    }
+
+    private void loadFromInternalClasspath() throws IOException, ClassNotFoundException, URISyntaxException {
+        String path = pluginNamespace.replace('.', '/');
+        //Get all Ressource in Classpath
+        Enumeration<URL> resources = bundledClassLoader.getResources(path);
+        while (resources.hasMoreElements()) {
+            URL classPath = resources.nextElement();
+            File file = new File(classPath.toURI());
+            loadPluginsFromDirectory(file);
+        }
+    }
+
+    private void loadPluginsFromDirectory(File file) throws ClassNotFoundException {
+        if (file.isDirectory()) {
+            //All Plugins in checked directory
+            File[] pluginRoot = file.listFiles();
+            for (File pluginDirectory : pluginRoot) {
+                if (pluginDirectory.isDirectory()) {
+                    loadFilesFromPlugin(pluginDirectory.listFiles(), pluginDirectory.getName());
+                }
+            }
+        }
+    }
+
+    private void loadFilesFromPlugin(File[] pluginFileList, String pluginName) throws ClassNotFoundException {
         boolean isFirstRecursiveStage = true;
         for (File pluginFile : pluginFileList
         ) {
             if (pluginFile.isDirectory()) {
 
                 //TODO NULL check for other implementations
-                loadFromBundle(pluginFile.listFiles(), pluginName);
+                loadFilesFromPlugin(pluginFile.listFiles(), pluginName);
                 isFirstRecursiveStage = false;
             } else {
 
                 String className = pluginNamespace + "." + pluginFile.getParentFile().getName() + "." + pluginFile.getName().substring(0, pluginFile.getName().length() - 6);
-
                 if (className.endsWith("Plugin") && pluginFile.getParentFile().getName().equals(pluginName)) {
-                    try {
-                        pluginList.add(pluginFactory.createPlugin(className));
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
+                    createPlugin(className);
                 }
             }
         }
     }
+
+    private void createPlugin(String className) {
+        try {
+            pluginList.add(pluginFactory.createPlugin(className));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 
     @Override
     public ICore init() {
